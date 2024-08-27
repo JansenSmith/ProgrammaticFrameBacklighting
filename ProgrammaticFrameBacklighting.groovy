256c8fa0dfd1c5f09bb10c9fa69b327b60692c2c
Jansen Smith <jansen@bstarengineering.com>
2024-08-27 14:49:43 -0400

    added battery box

--- ProgrammaticFrameBacklighting.groovy
+++ ProgrammaticFrameBacklighting.groovy
@@ -1,1 +1,7 @@
-// code here
+// ProgrammaticFrameBacklighting.groovy
+
+battery_x = 2.95*25.4
+battery_y = 1.9*25.4
+battery_z = 0.67*25.4
+
+CSG battery_box = new Cube(battery_x,battery_y, battery_z).toCSG()
