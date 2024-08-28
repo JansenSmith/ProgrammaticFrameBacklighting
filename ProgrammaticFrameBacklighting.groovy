36b4c5b7bb8ff63465d4d9190d552341ec9d7853
Jansen Smith <jansen@bstarengineering.com>
2024-08-28 18:56:31 -0400

    created mid piece

--- ProgrammaticFrameBacklighting.groovy
+++ ProgrammaticFrameBacklighting.groovy
@@ -3,7 +3,7 @@ import eu.mihosoft.vrl.v3d.*
 // ProgrammaticFrameBacklighting.groovy
 
 def painting_thickness_z = 6.1
-def painting_edge_y = 3
+def painting_edge_y = 4
 
 
 def led_cutout_y = 0.12*25.4 // too small? smallest possible
@@ -16,19 +16,19 @@ def side_thickness_y = 2
 def back_thickness_z = 3
 def front_thickness_z = 5
 
-def rim_y = 3
-
-def dovetail_y = 2
-
 def front_back_space_y = 0.1
 def front_back_space_z = 0.1
+
+def rim_y = 1.5
+def dovetail_y = 1
+def extra_y = painting_edge_y - dovetail_y - rim_y + front_back_space_y
 
 def inset_z = 2
 
 def section_x = 50
 
 def frame_back_x = section_x
-def frame_back_y = side_thickness_y + led_cutout_y + rim_y + dovetail_y
+def frame_back_y = side_thickness_y + led_cutout_y + rim_y + dovetail_y + extra_y
 def frame_back_z =  inset_z + painting_thickness_z + led_cutout_z + back_thickness_z
 
 def frame_front_x = section_x
@@ -71,17 +71,34 @@ CSG trench_frame_front = new Cube(frame_front_x, frame_front_y, front_thickness_
 								.toZMax()
 								.movez(frame_front_z)
 								.toYMin()
-trench_frame_front = trench_frame_front.union(new Cube(frame_front_x, side_thickness_y, frame_front_z).toCSG()
-								.toZMin()
-								.toYMax()
-								.movey(frame_front_y))
-trench_frame_front = trench_frame_front.union(new Cube(frame_front_x,led_cutout_y,frame_front_z-led_cutout_z-back_thickness_z-front_back_space_z).toCSG()
+trench_frame_front = trench_frame_front.union(new Cube(frame_front_x, led_cutout_y-front_back_space_y, painting_thickness_z+front_back_space_z+front_thickness_z).toCSG()
 								.toZMax()
 								.movez(frame_front_z)
 								.toYMax()
 								.movey(frame_back_y-side_thickness_y-front_back_space_y))
+trench_frame_front = trench_frame_front.union(new Cube(frame_front_x, side_thickness_y, frame_front_z).toCSG()
+								.toZMin()
+								.toYMax()
+								.movey(frame_front_y))
 // move to make y zero be the edge of diffusion paper
 trench_frame_front = trench_frame_front.movey(-dovetail_y)
+
+//middle spacer
+CSG trench_frame_mid = new Cube(frame_front_x,led_cutout_y+rim_y-front_back_space_y,frame_front_z-led_cutout_z-back_thickness_z-front_back_space_z*3-painting_thickness_z-front_thickness_z).toCSG()
+								.toZMax()
+								.movez(frame_front_z-front_back_space_z*2-painting_thickness_z-front_thickness_z)
+								.toYMax()
+								.movey(frame_back_y-side_thickness_y-front_back_space_y)
+trench_frame_mid = trench_frame_mid.union(new Wedge(rim_y-front_back_space_y, frame_front_x, lip_z).toCSG()
+								.rotz(270)
+								.rotx(180)
+								.toZMax()
+								.movez(led_cutout_z+back_thickness_z+front_back_space_z)
+								.toYMax()
+								.movey(frame_back_y-side_thickness_y-led_cutout_y-front_back_space_y)
+								.moveToCenterX())
+// move to make y zero be the edge of diffusion paper
+trench_frame_mid = trench_frame_mid.movey(-dovetail_y)
 
 // model the painting, for reference
 CSG painting = new Cube(section_x, painting_edge_y, painting_thickness_z).toCSG()
@@ -106,4 +123,4 @@ led = led.movey(-dovetail_y)
 CSG battery_box = new Cube(battery_x,battery_y, battery_z).toCSG()
 							.movex(300)
 
-return [trench_frame_back, trench_frame_front, led, painting]
+return [trench_frame_back, trench_frame_mid, trench_frame_front, led, painting]
