7851f77cf176e41ab3e63c8ee5d1dff14ca7af50
Jansen Smith <jansen@bstarengineering.com>
2024-09-07 15:16:40 -0400

    Making Cached SVG frame_slice.svg

--- ProgrammaticFrameBacklighting.groovy
+++ ProgrammaticFrameBacklighting.groovy
@@ -256,8 +256,8 @@ if(is_prototype_only)
 // code to slice the frame at X=0 and extrude 
 CSG frame_section = trench_frame_front.union(trench_frame_mid, led, painting, trench_frame_back) 
 
-def slice_trans = new Transform().roty(90).movez(1)
-//def slice_trans = new Transform().movez(10)
+//def slice_trans = new Transform().roty(90).movez(1)
+def slice_trans = new Transform().movez(10)
 List<Polygon> polys = Slice.slice(frame_section,slice_trans,0).collect{it.transformed(slice_trans)}
 
 String url = "https://github.com/JansenSmith/ProgrammaticFrameBacklighting.git"
